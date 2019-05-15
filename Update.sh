00cba58e52a1e6b6a77a153a2e43b5009f6c7132
Benito <benitoals@gmail.com>
2019-05-15 13:55:12 -0300

    Update path

--- Update.sh
+++ Update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Date: 2015-03-05
+# Date: 2019-05-15
 # To update GIT
 echo 
 echo -e "\e[01;31mUpdading git based applications...\e[00m"
@@ -17,11 +17,11 @@ rm "$f"
 
 echo 
 echo -e "\e[01;31mUpdading powershell modules...\e[00m"
-cd C:\\Users\\benit\\OneDrive\\Documentos\\WindowsPowerShell\\Modules
+cd C:\\Users\\User\\Path\\Documentos\\WindowsPowerShell\\Modules
 g="C:\\Windows\\Temp\\update2.tmp"
 ls -1 > "$g"
 while read l; do
-        cd "C:\\Users\\benit\\OneDrive\\Documentos\\WindowsPowerShell\\Modules\\$l";
+        cd "C:\\Users\\User\\Path\\Documentos\\WindowsPowerShell\\Modules\\$l";
         if [[ $(ls -1a | grep ".git" | wc -l) -gt 0 ]]; then
                 echo -e "-------\nUpdating $l module."
                 git pull;
